cbc5df7cd6bf910726f51a5994316b40df6a19e7
Oliver Fu <ofu997@gmail.com>
2021-10-04 14:50:53 -0700

    pass ItemModel and id to Item page

---
+++ learn-razor-pages/Pages/Item.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using learn_razor_pages.Models;
+using learn_razor_pages.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace learn_razor_pages.Pages
+{
+    public class ItemModel : PageModel
+    {
+        public string Message { get; set; }
+        public MenuItem Item { get; private set; }
+
+        public void OnGet(int id)
+        {
+            var menuService = new MenuService();
+            Item = menuService.GetMenuItems().FirstOrDefault(x => x.Id == id); 
+        }
+    }
+}
